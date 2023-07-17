529d590a2bcfa57221b98b91e3eca907e8b29251
chaitanyakumar167 <chaithanyakumar167@gmail.com>
2023-07-17 19:50:37 +0530

    added multiple users data and some improvements

--- main.js
+++ main.js
@@ -5,15 +5,18 @@ const number = document.getElementById("number");
 const date = document.getElementById("date");
 const time = document.getElementById("time");
 form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  let myobj = {
-    name: name1.value,
-    email: email.value,
-    number: number.value,
-    date: date.value,
-    time: time.value,
-  };
-  let myobj1 = JSON.stringify(myobj);
-  localStorage.setItem("myobj1", myobj1);
-  console.log(myobj1);
+  if (!form.checkValidity()) {
+    e.preventDefault();
+  } else {
+    let myobj = {
+      name: name1.value,
+      email: email.value,
+      number: number.value,
+      date: date.value,
+      time: time.value,
+    };
+    let myobj1 = JSON.stringify(myobj);
+    localStorage.setItem(email.value, myobj1);
+  }
+  form.classList.add("was-validated");
 });
